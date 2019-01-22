86b0e37974812657f261fc47da0868fadc1f352c
Klea Cengu <3cengk35@solent.ac.uk>
2019-01-22 15:28:16 +0000

    session1 and 2 upload

---
+++ AAA_MyClassWork/session1/HelloWorld/app/src/main/java/uk/ac/solent/helloworld/MainActivity.java
@@ -0,0 +1,19 @@
+package uk.ac.solent.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Create a TextView, and set it as the main content view of the activity
+        // TextView tv = new TextView(this);
+        // tv.setText("Hello World");
+        //setContentView(tv);
+    }
+}
